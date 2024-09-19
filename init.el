427ab327b6a08e0b7d79cda30a463a82ae922390
RoscoeEH <100191436+RoscoeEH@users.noreply.github.com>
2024-09-19 09:29:27 -0700

    key-chord package added

--- init.el
+++ init.el
@@ -193,4 +193,12 @@
 (global-set-key (kbd "s-C-f") 'toggle-fullscreen)
 
 
+;; key-chord to bind jk to escape
+(use-package key-chord
+  :ensure t
+  :config
+  (key-chord-mode 1))
+
+(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+
 
