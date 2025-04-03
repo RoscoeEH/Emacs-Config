3b00cf717f39359f6c65c327988832bf5b00b9ff
RoscoeEH <elingshaynier@gmail.com>
2025-04-03 14:06:21 -0400

    Fixed flyspell

--- package-configs/ispell-config.el
+++ package-configs/ispell-config.el
@@ -1,25 +1,24 @@
 ;;; ispell-config.el starts here
 
 (setq ispell-program-name "aspell")
+
 (add-hook 'text-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode) ;; Comments & strings only
+(add-hook 'markdown-mode-hook 'flyspell-mode) ;; Explicitly enable for Markdown
 
+;; Disable annoying messages but keep flyspell active
 (add-hook 'flyspell-mode-hook
-        (lambda ()
-            (setq flyspell-issue-message-flag nil)
-            (flyspell-mode-off))) ;; Prevent on-the-fly checks
+          (lambda ()
+            (setq flyspell-issue-message-flag nil)))
 
 (add-hook 'before-save-hook
-        (lambda ()
+          (lambda ()
             (when flyspell-mode
-            (flyspell-buffer))))
+              (flyspell-buffer))))
 
-(define-key evil-normal-state-map (kbd "SPC c c") 'ispell-word) ;; Correct word under the cursor
+(define-key evil-normal-state-map (kbd "SPC c c") 'ispell-word) ;; Correct word under cursor
 (define-key evil-normal-state-map (kbd "SPC c b") 'ispell-buffer) ;; Check entire buffer
 (define-key evil-visual-state-map (kbd "SPC c r") 'ispell-region) ;; Check selected region
-
-
-
 
 
 ;;; ispell-config.el ends here
