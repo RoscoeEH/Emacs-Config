c4e8732f19d82b78e2ed384489e33d5d658b0914
RoscoeEH <elingshaynier@gmail.com>
2025-05-15 10:40:51 -0700

    removed paste dwim

--- custom-commands.el
+++ custom-commands.el
@@ -198,48 +198,6 @@
                                             (evil-paste-before 1)))
 
 
-
-
-;; ;; paste preserves the spacing of the initial line
-;; (defun evil-paste-after-dwim (count &optional register yank-handler)
-;; (interactive "p")
-;; (let* ((start (point))
-;;         (initial-whitespace (save-excursion
-;;                                 (goto-char (line-beginning-position))
-;;                                 (when (looking-at "^[[:space:]]*")
-;;                                 (match-string 0)))))
-;;     ;; Paste the text
-;;     (evil-paste-after count register yank-handler)
-;;     ;; Align pasted text by adding initial whitespace to each subsequent line
-;;     (when initial-whitespace
-;;     (save-excursion
-;;         (goto-char start)
-;;         (while (re-search-forward "\n\\([^\n]\\)" nil t)
-;;         (replace-match (concat "\n" initial-whitespace "\\1")))))))
-
-
-;; (defun evil-paste-before-dwim (count &optional register yank-handler)
-;; (interactive "p")
-;; (let* ((start (point))
-;;         (initial-whitespace (save-excursion
-;;                                 (goto-char (line-beginning-position))
-;;                                 (when (looking-at "^[[:space:]]*")
-;;                                 (match-string 0)))))
-;;     ;; Paste before the cursor
-;;     (evil-paste-before count register yank-handler)
-;;     ;; Align pasted text by adding initial whitespace to each subsequent line
-;;     (when initial-whitespace
-;;     (save-excursion
-;;         (goto-char start)
-;;         (while (re-search-forward "\n\\([^\n]\\)" nil t)
-;;         (replace-match (concat "\n" initial-whitespace "\\1")))))))
-
-
-
-;; (define-key evil-normal-state-map (kbd "p") 'evil-paste-after-dwim)
-;; (define-key evil-normal-state-map (kbd "P") 'evil-paste-before-dwim)
-
-
 ;; Window management bindings with auto-balancing and window switching
 (defun split-window-horizontal-dwim ()
   "Splits the window horizontally then moves to the new window."
